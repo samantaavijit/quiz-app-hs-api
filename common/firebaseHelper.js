aa10fd5dfc7173f2a95fb575ce640ca802cdda28
samantaavijit <samantaavijit.2000@gmail.com>
2024-03-12 12:09:43 +0530

    changes new deployed

--- common/firebaseHelper.js
+++ common/firebaseHelper.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
   storageBucket: process.env.FIREBASE_BUCKET_URL,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
